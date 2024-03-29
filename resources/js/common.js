dea0d1bb25c1e8bc9758b6ece2e10a0dee7084b3
Kihwan <eljsh95@naver.com>
2024-03-29 18:41:27 +0900

    반응형작업중

--- resources/js/common.js
+++ resources/js/common.js
@@ -161,3 +161,12 @@ $(document).ready(function () {
     freemode: true,
   });
 });
+
+function copy(id) {
+  var r = document.createRange();
+  r.selectNode(document.getElementById(id));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+}
